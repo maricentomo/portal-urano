2067d204f4e0d9c37bb8e23c2d2aed152bc79c27
Mariana Centomo <mari.centomo@gmail.com>
2025-12-19 20:16:12 -0300

    use PORT env on startup

--- main.py
+++ main.py
@@ -1204,6 +1204,9 @@ async def generate_chart_svg_endpoint(request: ChartSVGRequest):
 
 
 if __name__ == "__main__":
+    import os
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.getenv("PORT", "8000"))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
